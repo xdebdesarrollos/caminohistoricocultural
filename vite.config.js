2ead876df7775ceb7da1e4e776ebc5a94fe74b6f
xdebdesarrollos <xdebdesarrollos@gmail.com>
2025-09-03 11:22:22 -0300

    Activando Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base:'https://xdebdesarrollos.github.io/caminohistoricocultural'
+  base:'https://xdebdesarrollos.github.io/caminohistoricocultural'
  // base:'http://localhost:5173/'
 })
