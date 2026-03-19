a94c4139ca35ba4c5f12b3c7cd5021d865d1273a
xdebdesarrollos <xdebdesarrollos@gmail.com>
2026-03-19 19:53:48 -0300

    Actualizado boton de map en Cristo-Parroquia - Salto

--- src/pages/CristoDeLaAmistad.jsx
+++ src/pages/CristoDeLaAmistad.jsx
@@ -1,15 +1,27 @@
 import Cabecera from "../components/CabeceraCristo.jsx";
 import Cuerpo from "../components/CuerpoCristo.jsx";
 import Pie from "../components/Pie.jsx";
+import "../css/btn-map.css";
 import React from 'react';
 
 import { motion } from "framer-motion";
 
 export default function CaminoCultural() {
+   const mapUrl = "https://maps.google.com"; // aquí luego pones la dirección real
   return (
     <>    
           <Cabecera/>  
           <Cuerpo/>
+          <div className="btn-map">
+            <a 
+              href={mapUrl} 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="map-button"
+              >
+              Ver ubicación en el mapa
+            </a>
+          </div>
           <Pie/>
     </>
 
