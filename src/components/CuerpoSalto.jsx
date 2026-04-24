108dfca2024b0343371b5bb7447af2b4dd1d6330
xdebdesarrollos <xdebdesarrollos@gmail.com>
2026-04-24 16:27:19 -0300

    Carga audio ubicacion de los saltos. Foto peñas ballet Joayhu

--- src/components/CuerpoSalto.jsx
+++ src/components/CuerpoSalto.jsx
@@ -44,6 +44,7 @@ export default function Cuerpo() {
         "./images/SaltosTabay/Turista1997.jpg",
         "./images/SaltosTabay/Turista1997Mereles.jpg",
         "./images/SaltosTabay/Penias2025.jpg",
+        "./images/SaltosTabay/Penias2026.jpg",
         "./images/SaltosTabay/Turista2024.jpg",
         "./images/SaltosTabay/Turista2026.jpg",
   ];
